55811957c3a846dd3ffd5888adedb99e1aa4cc1d
antonynans <antonynans@gmail.com>
2020-12-21 12:10:03 +0100

    added the pricing component

--- src/pages/products/Data.js
+++ src/pages/products/Data.js
@@ -1,15 +1,15 @@
 export const homeObjOne = {
     lightBg: true,
-    lightText: false,
+    lightText: true,
     lightTopLine: false,
-    lightTextDesc: false,
+    lightTextDesc: true,
     topLine: 'View Our Products',
-    headline: 'Shop through our catalog of products',
+    headLine: 'Shop through our catalog of products',
     description:
       'We provide worldwide shipping to all countries. If there are any issues, just issue a refund and we will process your request',
     buttonLabel: 'Shop Now',
     imgStart: '',
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/svg-1.svg').default,
     alt: 'Credit Card'
   };
   
@@ -19,12 +19,12 @@ export const homeObjOne = {
     lightTopLine: true,
     lightTextDesc: true,
     topLine: '100% Secure',
-    headline: 'Stay protected 24/7 anywhere anytime',
+    headLine: 'Stay protected 24/7 anywhere anytime',
     description:
       'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime',
     buttonLabel: 'Learn More',
     imgStart: '',
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/svg-3.svg').default,
     alt: 'Vault'
   };
   
@@ -33,12 +33,12 @@ export const homeObjOne = {
     lightText: false,
     lightTextDesc: false,
     topLine: 'Easy Setup',
-    headline: 'Super fast and simple onboarding process',
+    headLine: 'Super fast and simple onboarding process',
     description:
       "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
     buttonLabel: 'Start Now',
     imgStart: 'start',
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/svg-1.svg').default,
     alt: 'Vault'
   };
   
@@ -47,11 +47,11 @@ export const homeObjOne = {
     lightText: true,
     lightTextDesc: true,
     topLine: 'DATA ANALYTICS',
-    headline: 'Every transaction is stored on our secure cloud database',
+    headLine: 'Every transaction is stored on our secure cloud database',
     description:
       'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
     buttonLabel: 'Sign Up Now',
     imgStart: 'start',
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/svg-1.svg').default,
     alt: 'Vault'
   };
--- src/pages/signUp/Data.js
+++ src/pages/signUp/Data.js
@@ -4,12 +4,12 @@ export const homeObjOne = {
     lightTopLine: true,
     lightTextDesc: true,
     topLine: 'Sign up Today',
-    headline: 'Join today to receive an exclusive offer',
+    headLine: 'Join today to receive an exclusive offer',
     description:
       'Get access to our exclusive diamond king package. Limited quantity available.',
     buttonLabel: 'Sign Up',
     imgStart: '',
-    img: require('../../images/svg-2.svg'),
+    img: require('../../images/svg-2.svg').default,
     alt: 'Credit Card'
   };
   
@@ -19,12 +19,12 @@ export const homeObjOne = {
     lightTopLine: true,
     lightTextDesc: true,
     topLine: '100% Secure',
-    headline: 'Stay protected 24/7 anywhere anytime',
+    headLine: 'Stay protected 24/7 anywhere anytime',
     description:
       'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime',
     buttonLabel: 'Learn More',
     imgStart: '',
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/svg-1.svg').default,
     alt: 'Vault'
   };
   
@@ -34,12 +34,12 @@ export const homeObjOne = {
     lightTopLine: true,
     lightTextDesc: false,
     topLine: 'Easy Setup',
-    headline: 'Super fast and simple onboarding process',
+    headLine: 'Super fast and simple onboarding process',
     description:
       "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
     buttonLabel: 'Start Now',
     imgStart: 'start',
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/svg-1.svg').default,
     alt: 'Vault'
   };
   
@@ -49,11 +49,11 @@ export const homeObjOne = {
     lightTopLine: true,
     lightTextDesc: true,
     topLine: 'Data Analytics',
-    headline: 'Every transaction is stored on our secure cloud database',
+    headLine: 'Every transaction is stored on our secure cloud database',
     description:
       'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
     buttonLabel: 'Sign Up Now',
     imgStart: 'start',
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/svg-1.svg').default,
     alt: 'Vault'
   };
